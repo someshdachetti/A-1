3958482bb1774608c58edbad5e6f39c1266f3df4
someshdachetti <dssomesh201@gmail.com>
2023-04-17 03:49:10 +0000

    main

---
+++ public/img/components/AssignmentOutput/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import './index.css'
+
+class AssignmentOutput extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Somesh</h1>
+      </div>
+    )
+  }
+}
+export default AssignmentOutput
